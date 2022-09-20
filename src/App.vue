1fc2c7b72a9874eefb5a32f0991b644a44ace31d
Lucas Roseti <lc.roseti@gmail.com>
2022-09-19 23:08:44 -0300

    Conditions
    - Box component with slot
    - Condition when task description is empty
    - Condition when tasks list is empty

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 
       <div class="lista">
         <Task v-for="(task, index) in tasks" :key="index" :task="task" />
+        <Box v-if="listIsEmpty">Você não está muito produtivo hoje!</Box>
       </div>
     </div>
   </main>
@@ -15,6 +16,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Box from './components/Box.vue'
 import Form from './components/Form.vue'
 import SideBar from './components/SideBar.vue'
 import Task from './components/Task.vue'
@@ -23,10 +25,15 @@ import ITask from './interfaces/ITask'
 
 export default defineComponent({
   name: 'App',
-  components: { Form, SideBar, Task },
+  components: { Box, Form, SideBar, Task },
   data () {
     return {
       tasks: [] as ITask[]
+    }
+  },
+  computed: {
+    listIsEmpty () : boolean {
+      return this.tasks.length === 0
     }
   },
   methods: {
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <Box>
     <div class="columns">
-      <div class="column is-7">{{task.description}}</div>
+      <div class="column is-7">{{task.description || 'Tarefa sem descrição'}}</div>
       <div class="column">
         <Timer :timeInSeconds="task.duration" />
       </div>
     </div>
-  </div>
+  </Box>
 </template>
 
 <script lang="ts">
   import { defineComponent, PropType } from 'vue'
+  import Box from './Box.vue'
   import Timer from './Timer.vue'
   import ITask from '../interfaces/ITask'
 
   export default defineComponent({
     name: 'TaskTracker',
-    components: { Timer },
+    components: { Box, Timer },
     props: {
       task: { type: Object as PropType<ITask>, required: true }
     }
